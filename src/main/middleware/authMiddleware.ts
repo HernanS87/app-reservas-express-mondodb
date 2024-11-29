c9afca25dfeecc31cbd597b49987e7d23c02585d
Hernan Santarelli <hernansantarelli87@gmail.com>
2024-11-29 13:49:29 -0300

    config para deploy 2

--- src/main/middleware/authMiddleware.ts
+++ src/main/middleware/authMiddleware.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
-import { IAuthTokenPayload } from "main/model/interface/authInterface";
+import { IAuthTokenPayload } from "../model/interface/authInterface";
 
 // Middleware para autenticar el token
 export function authenticateToken(req: Request, res: Response, next: NextFunction): void {
